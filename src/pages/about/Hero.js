5e48e7b66c5c7a098cfe52faa9f79905242b0943
Jose Riera Tur <rieraturjose@gmail.com>
2023-05-07 23:02:20 +0200

    change quality of about picture and metadata from manifest

--- src/pages/about/Hero.js
+++ src/pages/about/Hero.js
@@ -1,24 +1,27 @@
 import "./Hero.css";
-import "../MediaQueries.css"
+import "../MediaQueries.css";
+import { Suspense } from "react";
 
 function Hero() {
   return (
     <div className="about-hero">
-      <picture>
-        <source
-          srcSet={require("../../img/about-salute.webp")}
-          type="image/webp"
-        />
-        <source
-          srcSet={"../../img/backup-pngs/about-salute.png"}
-          type="image/png"
-        />
-        <img
-          src="../../img/backup-pngs/about-salute.png"
-          alt="a picture of a beautiful man sitting with a computer next to flowers"
-          className="about-hero-left-img"
-        ></img>
-      </picture>
+      <Suspense fallback={<p>Loading...</p>}>
+        <picture>
+          <source
+            srcSet={require("../../img/about-salute.webp")}
+            type="image/webp"
+          />
+          <source
+            srcSet={"../../img/backup-pngs/about-salute.png"}
+            type="image/png"
+          />
+          <img
+            src="../../img/backup-pngs/about-salute.png"
+            alt="a picture of a beautiful man sitting with a computer next to flowers"
+            className="about-hero-left-img"
+          ></img>
+        </picture>
+      </Suspense>
       <div className="about-hero-right">
         <p>
           My name is <strong>Jose Riera Tur</strong> and I welcome you to my{" "}
